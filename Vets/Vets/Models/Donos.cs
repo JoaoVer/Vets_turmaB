7597b740bfd9098ebfda23b869a548295afa4b86
JoaoVerissimo <joao17andre@hotmail.com>
2020-03-31 16:12:48 +0100

    Adição das FKs e das PKs às 'tabelas'

--- Vets/Vets/Models/Donos.cs
+++ Vets/Vets/Models/Donos.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,17 @@ namespace Vets.Models
 {
     public class Donos
     {
+        public Donos()
+        {
+            Animais = new HashSet<Animais>();
+        }
+
+        [Key]
         public int ID { get; set; }
         public string Nome { get; set; }
         public int NIF { get; set; }
+
+        // Lista de Animais de um determinado dono
+        public ICollection<Animais> Animais { get; set; }
     }
 }
