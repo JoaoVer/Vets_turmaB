0e55480f6968a33db2f418799441c9b9798e5aa1
JoaoVerissimo <joao17andre@hotmail.com>
2020-04-21 14:54:00 +0100

    Commit teste

--- Vets/Vets/Models/Veterinarios.cs
+++ Vets/Vets/Models/Veterinarios.cs
@@ -4,31 +4,30 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Vets.Models
-{
-    public class Veterinarios
-    {
-        public Veterinarios()
-        {
-            // Estou a colocar dados na lista
-            // na prática é como se fizesse
-            // Consulas = SELECT *
-            // FROM Consultas c, Veterinarios v
-            // WHERE c.VeterinarioFK = v.ID AND v.ID = ;
-            Consultas = new HashSet<Consultas>();
-        }
+namespace Vets.Models {
+   public class Veterinarios {
 
-        [Key]
-        public int ID { get; set; }
+      public Veterinarios() {
+         // estou a colocar dados na lista
+         // na prática é como se fizesse
+         // Consultas = SELECT * 
+         //             FROM Consultas c, Veterinarios v 
+         //             WHERE c.VeterinarioFK = v.ID AND
+         //                   v.ID = ?   ;
+         Consultas = new HashSet<Consultas>();
+      }
 
-        public string Nome { get; set; }
+      [Key]
+      public int ID { get; set; }
 
-        public string NumCedulaProf { get; set; }
+      public string Nome { get; set; }
 
-        public string Fotografia { get; set; }
+      public string NumCedulaProf { get; set; }
 
-        // Lista das Consulta a que um Veterinário está associado
+      public string Fotografia { get; set; }
 
-        public ICollection<Consultas> Consultas { get; set; }
-    }
+      // lista das Consulta a q um Veterinário está associado
+      public ICollection<Consultas> Consultas { get; set; }
+
+   }
 }
